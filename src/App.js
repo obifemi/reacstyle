7d72e804fb2b3da46c6ead9a23645082b51b5f09
ali mert inal <mert.inal@gmail.com>
2023-06-09 18:07:26 +0200

    add first

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
     { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
+    { text: 'Finish the course!', id: 'g2' } 
   ]);
 
   const addGoalHandler = enteredText => {
