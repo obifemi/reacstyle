ac60ceadbfe87d424d495ef710b56dbd2f0016d1
ali mert inal <mert.inal@gmail.com>
2023-06-10 12:01:00 +0200

    add toggle

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,32 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setisValid]  = useState(true)
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setisValid(true)
+    }
+    
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setisValid(false)
+      return;
+    };
+    setisValid(true)
+   
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color : isValid ? '#ccc' : 'red'}}>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} style={{backgroundColor: isValid ? '#ccc' : 'red'}} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
